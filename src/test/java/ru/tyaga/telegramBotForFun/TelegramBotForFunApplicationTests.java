67cff6d64013b0517ced0fb4fd1d84fdcfa890cb
Tom <Thomasvill16@inbox.ru>
2024-03-15 23:07:59 +0400

    Создание приложение

---
+++ src/test/java/ru/tyaga/telegramBotForFun/TelegramBotForFunApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tyaga.telegramBotForFun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
